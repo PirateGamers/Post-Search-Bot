a9a308b3d5b67c5afbc1babe863da77d89675ca1
PirateGamers <109447641+PirateGamers@users.noreply.github.com>
2025-03-31 19:23:53 +0530

    Update script.py

--- utils/script.py
+++ utils/script.py
@@ -28,7 +28,7 @@ class script(object):
     ABOUT = """<b>╭━━━━━━━❰ [𝙰𝙱𝙾𝚄𝚃](https://t.me/TechnicalBichu) ❱━━━━━━━➣
 
 ┣ ✯ Mʏ Nᴀᴍᴇ:  {}
-┣ ✯ Cʀᴇᴀᴛᴏʀ: <a href='https://t.me/PirateGamers>PirateGamers</a>
+┣ ✯ Cʀᴇᴀᴛᴏʀ: <a href='https://t.me/PirateGamers'>PirateGamers</a>
 ┣ ✯ Lᴀɴɢᴜᴀɢᴇ: <a href='https://www.python.org'>Pʏᴛʜᴏɴ 3</a>
 ┣ ✯ DᴀᴛᴀBᴀsᴇ: <a href='https://cloud.mongodb.com/'>MᴏɴɢᴏDB</a></b>"""
 
